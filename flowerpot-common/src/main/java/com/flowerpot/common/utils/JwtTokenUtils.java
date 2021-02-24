fefb2e745fbb037db5799a1131e34114459d92ec
hanhao <1309443685@qq.com>
2021-02-24 11:15:40 +0800

    Result

---
+++ flowerpot-common/src/main/java/com/flowerpot/common/utils/JwtTokenUtils.java
@@ -0,0 +1,13 @@
+package com.flowerpot.common.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * JwtTokenUtils
+ *
+ * @author Mrhan
+ * @date 2021/2/24 10:57
+ */
+@UtilityClass
+public class JwtTokenUtils {
+}
