068c3141ff311f47e71c0f6ac0f1751dc5d45063
mrhanhao <1309443685@qq.com>
2021-04-07 14:28:23 +0800

    邮件 Dao层

---
+++ flowerpot-service/flowerpot-mailbox-service/flowerpot-mailbox-service-dao/src/main/java/com/flowerpot/service/mailbox/mapper/EmailMessageMapper.java
@@ -0,0 +1,13 @@
+package com.flowerpot.service.mailbox.mapper;
+
+import com.flowerpot.common.model.BaseMapper;
+import com.flowerpot.service.mailbox.service.entity.EmailMessage;
+
+/**
+ * EmailMessageMapper
+ *
+ * @author Mrhan
+ * @date 2021/4/7 14:26
+ */
+public interface EmailMessageMapper extends BaseMapper<EmailMessage> {
+}
