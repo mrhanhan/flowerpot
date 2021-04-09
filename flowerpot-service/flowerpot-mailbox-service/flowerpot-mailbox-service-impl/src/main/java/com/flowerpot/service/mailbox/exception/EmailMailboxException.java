a1efcf57f3292b72c5f6dc011c063087fdd2b1f1
mrhanhao <1309443685@qq.com>
2021-04-09 10:08:39 +0800

    电子邮件服务

---
+++ flowerpot-service/flowerpot-mailbox-service/flowerpot-mailbox-service-impl/src/main/java/com/flowerpot/service/mailbox/exception/EmailMailboxException.java
@@ -0,0 +1,28 @@
+package com.flowerpot.service.mailbox.exception;
+
+/**
+ * EmailMailboxException
+ * 电子邮件邮箱异常
+ * @author Mrhan
+ * @date 2021/4/9 10:06
+ */
+public class EmailMailboxException extends RuntimeException{
+    public EmailMailboxException() {
+    }
+
+    public EmailMailboxException(String message) {
+        super(message);
+    }
+
+    public EmailMailboxException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmailMailboxException(Throwable cause) {
+        super(cause);
+    }
+
+    public EmailMailboxException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
--- flowerpot-service/flowerpot-mailbox-service/flowerpot-mailbox-service-impl/src/main/java/com/flowerpot/service/mailbox/impl/MailboxServiceImpl.java
+++ flowerpot-service/flowerpot-mailbox-service/flowerpot-mailbox-service-impl/src/main/java/com/flowerpot/service/mailbox/impl/MailboxServiceImpl.java
@@ -1,26 +1,36 @@
 package com.flowerpot.service.mailbox.impl;
 
+import com.baomidou.mybatisplus.extension.conditions.update.LambdaUpdateChainWrapper;
+import com.flowerpot.common.model.BaseEntity;
 import com.flowerpot.common.utils.Asset;
 import com.flowerpot.service.mailbox.dto.EmailMessageBo;
+import com.flowerpot.service.mailbox.exception.EmailMailboxException;
 import com.flowerpot.service.mailbox.provider.EmailMailboxProvider;
 import com.flowerpot.service.mailbox.provider.EmailMessageSenderProvider;
 import com.flowerpot.service.mailbox.service.EmailMailboxService;
+import com.flowerpot.service.mailbox.service.EmailMessageContentService;
+import com.flowerpot.service.mailbox.service.EmailMessageService;
 import com.flowerpot.service.mailbox.service.MailboxService;
 import com.flowerpot.service.mailbox.service.entity.EmailMailbox;
 import com.flowerpot.service.mailbox.service.entity.EmailMessage;
 import com.flowerpot.service.mailbox.service.entity.EmailMessageContent;
 import com.flowerpot.service.mailbox.service.enums.EmailMailboxEnum;
+import com.flowerpot.service.mailbox.service.enums.EmailStatusEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * MailboxServiceImpl
@@ -28,6 +38,7 @@ import java.util.Objects;
  * @author Mrhan
  * @date 2021/4/7 15:05
  */
+@Slf4j
 @Service
 public class MailboxServiceImpl implements MailboxService {
 
@@ -35,26 +46,76 @@ public class MailboxServiceImpl implements MailboxService {
     private EmailMessageSenderProvider emailMessageSenderProvider;
     @Resource
     private EmailMailboxService emailMailboxService;
+    @Resource
+    private EmailMessageService emailMessageService;
+    @Resource
+    private EmailMessageContentService emailMessageContentService;
     /**
      * 电子邮箱服务提供者
      */
-    private final List<EmailMailboxProvider> emailMailboxProviderList;
-
-    public MailboxServiceImpl(@Autowired(required = false) List<EmailMailboxProvider> emailMailboxProviderList) {
-        this.emailMailboxProviderList = emailMailboxProviderList;
-    }
+    @Autowired(required = false)
+    private List<EmailMailboxProvider> emailMailboxProviderList;
 
     @Override
     public void sendEmail(EmailMessageBo message) {
         // 获取电子邮件
-        // Email Mailbox
         EmailMailbox emailMailbox = getEmailMailbox(message.getMailbox());
+        // 保存对象数据
+        saveEmailData(emailMailbox, message);
+        EmailMessage emailMessage = message.getMessage();
+        // 发送电子邮件
+        Consumer<EmailStatusEnum> updateEmailMessageStatusFunc = (status) -> {
+            LambdaUpdateChainWrapper<EmailMessage> wrapper = emailMessageService.lambdaUpdate();
+            wrapper.set(EmailMessage::getStatus, EmailStatusEnum.SEND_SUCCESS.getKey()).eq(BaseEntity::getId, emailMailbox.getId());
+            wrapper.update();
+        };
+        try {
+            sendMail(emailMailbox, message);
+            // 更新状态
+            updateEmailMessageStatusFunc.accept(EmailStatusEnum.SEND_SUCCESS);
+        }
+        catch (EmailMailboxException e) {
+            log.error("邮件发送失败:", e);
+            updateEmailMessageStatusFunc.accept(EmailStatusEnum.SEND_EXCEPTION);
+        }
+        catch (Exception e) {
+            log.error("邮件发送失败:", e);
+            updateEmailMessageStatusFunc.accept(EmailStatusEnum.SEND_FAIL);
+        }
+    }
+
+    /**
+     * 保存电子邮件数据
+     * @param emailMailbox  电子邮件邮箱
+     * @param message       电子邮件消息
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public void saveEmailData(EmailMailbox emailMailbox, EmailMessageBo message) {
+        EmailMessage emailMessage = message.getMessage();
+        EmailMessageContent emailMessageContent = message.getContent();
+        emailMessage.setMailboxId(emailMailbox.getId());
+
+        emailMessageService.save(emailMessage);
+        emailMessageContentService.save(emailMessageContent);
+    }
+
+
+    /**
+     * 发送电子邮件
+     * @param emailMailbox  EmailMessageBox
+     * @param message       消息
+     */
+    private void sendMail(EmailMailbox emailMailbox, EmailMessageBo message) {
         // 获取邮件发送服务
         MailSender sender = getMailSender(emailMailbox);
         // 邮件消息
         SimpleMailMessage simpleMailMessage = createSimpleMailMessage(message, emailMailbox);
         // 发送邮件
-        sender.send(simpleMailMessage);
+        try {
+            sender.send(simpleMailMessage);
+        } catch (MailAuthenticationException e) {
+            throw new EmailMailboxException(e);
+        }
     }
 
     /**
